6524d1b259e83868add3716c42ff800e27fa65e6
Rishika Agarwal <122405415+Rishika-993@users.noreply.github.com>
2023-10-08 22:19:34 +0530

    traversing

---
+++ LinkedListTraversal.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>  //for malloc
+struct Node
+{
+    int data;
+    struct Node * next;
+    
+};
+int main(){
+    struct Node * head;
+    struct Node * second;
+    struct Node * third;
+    // Allocate memory for nodes in the linked list in heap
+    head = (struct Node *) malloc(sizeof(struct Node
+    ));
+    second = (struct Node *) malloc(sizeof(struct Node
+    ));
+    third = (struct Node *) malloc(sizeof(struct Node
+    ));
+
+    // Link first and second nodes
+    head -> data = 7;
+    head -> next = second;
+
+    // Link second and third nodes
+    second -> data = 11;
+    second -> next = third;
+
+    // Terminate the list at the third node
+    third -> data = 66;
+    third -> next = NULL;
+
+    return 0;
+}
