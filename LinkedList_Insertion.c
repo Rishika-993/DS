3e9cc85eed8f0309d27d06db1c69441cab38ab16
Rishika Agarwal <122405415+Rishika-993@users.noreply.github.com>
2023-10-13 23:53:27 +0530

    Update LinkedList_Insertion.c

--- LinkedList_Insertion.c
+++ LinkedList_Insertion.c
@@ -1,1 +1,12 @@
+// Insert at begining 
+
+struct Node * insertionAtFirst(struct Node *head , int data){
+    struct Node *ptr;
+    ptr = (struct Node *) malloc(sizeof(struct Node ));
+    ptr->next = head;
+    ptr->data = data;
+    return ptr;
+}
+
+head = insertionAtFirst(head, 56);
 
