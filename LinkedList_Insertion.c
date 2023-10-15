f070ecce6cefdee5525b18d09ef0fd2c7e05712f
Rishika Agarwal <122405415+Rishika-993@users.noreply.github.com>
2023-10-15 22:57:21 +0530

    Update LinkedList_Insertion.c

--- LinkedList_Insertion.c
+++ LinkedList_Insertion.c
@@ -10,4 +10,19 @@ struct Node * insertionAtFirst(struct Node *head , int data){
 
 head = insertionAtFirst(head, 56);
 
+//Insertion At Index
+struct Node *insertAtindex(struct Node *head, int data, int index){
+    struct Node *ptr = (struct Node *) malloc(sizeof(struct Node ));
+    struct Node *p = head;
+    int i=0;
+    while(i!=index){
+        p=p->next;
+        i++;
+    }
+    ptr->data = data;
+    ptr->next=p->next;
+    p->next = ptr;
+    return head;
+}
+head = insertionAtFirst(head, 56, 1);
 // Insert at end
