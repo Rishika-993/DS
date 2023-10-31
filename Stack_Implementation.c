f98572369931efdc6e2445d71f4b3164f49908eb
Rishika Agarwal <122405415+Rishika-993@users.noreply.github.com>
2023-10-31 11:17:07 +0530

    Stack

---
+++ Stack_Implementation.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define  size 4
+int top = -1, arr[size];
+void push(int val){
+    if(top == size-1){
+        printf("Overflow");
+    }
+    else{
+        top = top+1;
+        arr[top]= val;
+    }
+}
+void pop(){
+    if(top == -1){
+        printf("Underflow");
+    }
+    else{
+        printf("Popped item %d", arr[top]);
+        top = top-1;
+    }
+}
+int main(){
+    push(23);
+    push(8);
+    push(89);
+    pop();
+    printf("\nItems in Stack are:");
+    for(int i=0; i<size; i++){
+        printf("\n%d",arr[i]);
+    }
+}
