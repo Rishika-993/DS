03b82c18aa2554b264b86dbf419e968fde8fce51
Rishika Agarwal <122405415+Rishika-993@users.noreply.github.com>
2023-11-02 21:41:20 +0530

    Operations

---
+++ ArrayOperation.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int nums[100], n, i, s, k, j;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    for (i = 0; i < n; i++) {
+        if(nums[i] == nums[i+1]){
+            nums[i]=nums[i]*2;
+            nums[i+1]= 0;
+        }
+        if(nums[i]==0){
+            for(j=i; j<n; j++){
+                nums[j]=nums[j+1];
+            }
+            nums[n - 1] = 0;
+            i--; 
+            n--; 
+        }
+    } 
+    printf("%d",n);
+    for (i = 0; i < 5; i++) {
+        printf("%d", nums[i]);     
+    }
+    return 0; 
+}  
